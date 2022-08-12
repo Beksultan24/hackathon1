0838da5df9a82f441aac6121c9b2b53d7263218a
Beksultan24 <beksbor05@gmail.com>
2022-08-12 16:46:20 +0300

    papki

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+      <Cart />
+    </>
+  );
 }
 
 export default App;
